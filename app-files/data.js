2e4458275a3df8a31e76f2948f127b45808d3af8
Nahu94 <nletang@itba.edu.ar>
2022-08-03 11:55:27 -0300

    Carga archivos

---
+++ app-files/data.js
@@ -0,0 +1,94 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-1",
+      "name": "1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -1.761357785941211,
+        "pitch": 0.3259947265103804,
+        "fov": 1.2809123973965084
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.019932237261062,
+          "pitch": 0.46598917158187625,
+          "rotation": 13.351768777756625,
+          "target": "1-2"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 1.3411145837534981,
+          "pitch": 0.9623855765981304,
+          "title": "Mancha",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "1-2",
+      "name": "2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -2.7304692799060533,
+        "pitch": 0.3499128743358604,
+        "fov": 1.2809123973965084
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.16394527756742505,
+          "pitch": 0.5914221025159776,
+          "rotation": 0,
+          "target": "0-1"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Casa Test",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
